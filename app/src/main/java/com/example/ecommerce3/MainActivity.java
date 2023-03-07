64a2fc7fe9c03ffba62f1b776383f96d6e75affc
anagorea <gorea.ann@gmail.com>
2023-03-07 20:19:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ecommerce3/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ecommerce3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        joinNowButton = (Button) findViewById(R.id.main_joi_now_btn);
+        joinNowButton.getBackground().setAlpha(150);  // 25% transparent
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
