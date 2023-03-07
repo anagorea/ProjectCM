9d1164fec08bde11c791c432e792705127ec3c38
anagorea <gorea.ann@gmail.com>
2023-03-07 22:24:05 +0200

    commit2

--- app/src/main/java/com/example/ecommerce3/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce3/LoginActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+//first comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
