1537010ddecb98c09864856c6c0d5cdfa3d55db1
anagorea <gorea.ann@gmail.com>
2023-04-24 10:57:21 +0300

    commit11

---
+++ app/src/main/java/com/example/ecommerce3/activities/NavCategoryActivity.java
@@ -0,0 +1,65 @@
+package com.example.ecommerce3.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.ecommerce3.R;
+import com.example.ecommerce3.adapters.NavCategoryDetailedAdapter;
+import com.example.ecommerce3.models.HomeCategory;
+import com.example.ecommerce3.models.NavCategoryDetailedModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NavCategoryActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<NavCategoryDetailedModel> list;
+    NavCategoryDetailedAdapter adapter;
+    FirebaseFirestore db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_category);
+
+        db = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.nav_cat_det_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
+        adapter = new NavCategoryDetailedAdapter(this,list);
+        recyclerView.setAdapter(adapter);
+
+        db.collection("NavCategoryDetailed")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                NavCategoryDetailedModel navCategoryDetailedModel = document.toObject(NavCategoryDetailedModel.class);
+                                list.add(navCategoryDetailedModel);
+                                adapter.notifyDataSetChanged();
+
+                            }
+                        } else {
+
+                            Toast.makeText(NavCategoryActivity.this,"Error"+task.getException(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+}
